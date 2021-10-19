6db069cd4902c84b65a3af81667beec457e3192e
Alexandre Bintz <alexandre@bintz.io>
2021-10-19 19:26:09 +0200

    time slots intersections

--- Planning/Models/TimeSlot.swift
+++ Planning/Models/TimeSlot.swift
@@ -18,6 +18,16 @@ import Foundation
 /// print(timeSlot.endDate) // Date() + 2 seconds
 /// ```
 ///
+/// Time slots can check if they intersect with each other, and even return a time slot that corresponds the their common portion.
+///
+/// ```swift
+/// let timeSlot1 = TimeSlot(between: Date(timeIntervalSinceReferenceDate: 0), and: Date(timeIntervalSinceReferenceDate: 2))
+/// let timeSlot2 = TimeSlot(between: Date(timeIntervalSinceReferenceDate: 1), and: Date(timeIntervalSinceReferenceDate: 3))
+///
+/// timeSlot1.intersects(with: timeSlot2)  // true
+/// timeSlot1.intersection(with: timeSlot2) // TimeSlot(between: Date(timeIntervalSinceReferenceDate: 1), and: Date(timeIntervalSinceReferenceDate: 2))
+/// ```
+///
 struct TimeSlot: Codable, Equatable
 {
     
@@ -52,5 +62,54 @@ struct TimeSlot: Codable, Equatable
     {
         self.startDate = startDate
         self.duration = endDate.timeIntervalSince(startDate)
+    }
+    
+    
+    
+    /// Returns the common portion, if any, of two time slots.
+    ///
+    /// If the given time slots overlap, this method returns a time slot that correspond to their common portion.
+    ///
+    /// ```swift
+    /// let timeSlot1 = TimeSlot(between: Date(timeIntervalSinceReferenceDate: 0), and: Date(timeIntervalSinceReferenceDate: 2))
+    /// let timeSlot2 = TimeSlot(between: Date(timeIntervalSinceReferenceDate: 1), and: Date(timeIntervalSinceReferenceDate: 3))
+    ///
+    /// TimeSlot.intersection(between: timeSlot1, and: timeSlot2)  // TimeSlot(between: Date(timeIntervalSinceReferenceDate: 1), and: Date(timeIntervalSinceReferenceDate: 2))
+    ///
+    /// let timeSlot3 = TimeSlot(between: Date(timeIntervalSinceReferenceDate: 0), and: Date(timeIntervalSinceReferenceDate: 1))
+    /// let timeSlot4 = TimeSlot(between: Date(timeIntervalSinceReferenceDate: 2), and: Date(timeIntervalSinceReferenceDate: 3))
+    ///
+    /// TimeSlot.intersection(between: timeSlot3, and: timeSlot4))  // nil
+    /// ```
+    ///
+    /// Note that `intersection(between: timeSlot1, and: timeSlot2)` always produces the same result than `intersection(between: timeSlot2, and: timeSlot1)`.
+    /// In other words, the order in which the time slots are given has no effect.
+    ///
+    static func intersection(between timeSlot1: TimeSlot, and timeSlot2: TimeSlot) -> TimeSlot? {
+        
+        let latestStartDate = [timeSlot1.startDate, timeSlot2.startDate].max()!
+        let earliestEndDate = [timeSlot1.endDate, timeSlot2.endDate].min()!
+        
+        return latestStartDate < earliestEndDate ? TimeSlot(between: latestStartDate, and: earliestEndDate) : nil
+    }
+    
+    
+    /// Returns the common portion, if any, that this instance shares with another time slot.
+    ///
+    /// See `intersection(between:and:)` for more details about time slots intersections.
+    ///
+    func intersection(with otherTimeSlot: TimeSlot) -> TimeSlot? {
+        
+        return Self.intersection(between: self, and: otherTimeSlot)
+    }
+    
+    
+    /// Returns whether this instance shares a common portion with another time slot.
+    ///
+    /// See `intersection(between:and:)` for more details about time slots intersections.
+    ///
+    func intersects(with otherTimeSlot: TimeSlot) -> Bool {
+        
+        return intersection(with: otherTimeSlot) != nil
     }
 }
--- PlanningTest/TimeSlotTest.swift
+++ PlanningTest/TimeSlotTest.swift
@@ -57,4 +57,58 @@ class TimeSlotTest: XCTestCase {
         
         XCTAssertNotEqual(timeSlot1, timeSlot2)
     }
+    
+    
+    func test_intersection_noIntersection() {
+        
+        let timeSlot1 = TimeSlot(between: Date(timeIntervalSinceReferenceDate: 0), and: Date(timeIntervalSinceReferenceDate: 1))
+        let timeSlot2 = TimeSlot(between: Date(timeIntervalSinceReferenceDate: 2), and: Date(timeIntervalSinceReferenceDate: 3))
+        
+        let expectedIntersection: TimeSlot? = nil
+        
+        XCTAssertEqual(TimeSlot.intersection(between: timeSlot1, and: timeSlot2), expectedIntersection)
+        XCTAssertEqual(TimeSlot.intersection(between: timeSlot2, and: timeSlot1), expectedIntersection)
+        
+        XCTAssertEqual(timeSlot1.intersection(with: timeSlot2), expectedIntersection)
+        XCTAssertEqual(timeSlot2.intersection(with: timeSlot1), expectedIntersection)
+        
+        XCTAssertEqual(timeSlot1.intersects(with: timeSlot2), expectedIntersection != nil)
+        XCTAssertEqual(timeSlot2.intersects(with: timeSlot1), expectedIntersection != nil)
+    }
+    
+    
+    func test_intersection_oneContainsTheOther() {
+        
+        let timeSlot1 = TimeSlot(between: Date(timeIntervalSinceReferenceDate: 0), and: Date(timeIntervalSinceReferenceDate: 3))
+        let timeSlot2 = TimeSlot(between: Date(timeIntervalSinceReferenceDate: 1), and: Date(timeIntervalSinceReferenceDate: 2))
+        
+        let expectedIntersection: TimeSlot? = TimeSlot(between: Date(timeIntervalSinceReferenceDate: 1), and: Date(timeIntervalSinceReferenceDate: 2))
+        
+        XCTAssertEqual(TimeSlot.intersection(between: timeSlot1, and: timeSlot2), expectedIntersection)
+        XCTAssertEqual(TimeSlot.intersection(between: timeSlot2, and: timeSlot1), expectedIntersection)
+        
+        XCTAssertEqual(timeSlot1.intersection(with: timeSlot2), expectedIntersection)
+        XCTAssertEqual(timeSlot2.intersection(with: timeSlot1), expectedIntersection)
+        
+        XCTAssertEqual(timeSlot1.intersects(with: timeSlot2), expectedIntersection != nil)
+        XCTAssertEqual(timeSlot2.intersects(with: timeSlot1), expectedIntersection != nil)
+    }
+    
+    
+    func test_intersection_partialIntersection() {
+        
+        let timeSlot1 = TimeSlot(between: Date(timeIntervalSinceReferenceDate: 0), and: Date(timeIntervalSinceReferenceDate: 2))
+        let timeSlot2 = TimeSlot(between: Date(timeIntervalSinceReferenceDate: 1), and: Date(timeIntervalSinceReferenceDate: 3))
+        
+        let expectedIntersection: TimeSlot? = TimeSlot(between: Date(timeIntervalSinceReferenceDate: 1), and: Date(timeIntervalSinceReferenceDate: 2))
+        
+        XCTAssertEqual(TimeSlot.intersection(between: timeSlot1, and: timeSlot2), expectedIntersection)
+        XCTAssertEqual(TimeSlot.intersection(between: timeSlot2, and: timeSlot1), expectedIntersection)
+        
+        XCTAssertEqual(timeSlot1.intersection(with: timeSlot2), expectedIntersection)
+        XCTAssertEqual(timeSlot2.intersection(with: timeSlot1), expectedIntersection)
+        
+        XCTAssertEqual(timeSlot1.intersects(with: timeSlot2), expectedIntersection != nil)
+        XCTAssertEqual(timeSlot2.intersects(with: timeSlot1), expectedIntersection != nil)
+    }
 }
