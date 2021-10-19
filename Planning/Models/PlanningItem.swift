83bda8a52158ca35d0e44df24a4d36dda746d444
Alexandre Bintz <alexandre@bintz.io>
2021-10-19 11:24:47 +0200

    add ids

--- Planning/Models/PlanningItem.swift
+++ Planning/Models/PlanningItem.swift
@@ -5,8 +5,17 @@ import Foundation
 
 struct PlanningItem: Codable
 {
+    let id: UUID
     let task: Task
     let startDate: Date
     let duration: TimeInterval
     var endDate: Date { self.startDate.addingTimeInterval(self.duration) }
+    
+    init(withTask task: Task, startingAt startDate: Date, duration: TimeInterval) {
+        
+        self.id = UUID()
+        self.task = task
+        self.startDate = startDate
+        self.duration = duration
+    }
 }
--- Planning/Models/Task.swift
+++ Planning/Models/Task.swift
@@ -5,5 +5,12 @@ import Foundation
 
 struct Task: Codable
 {
+    let id: UUID
     let name: String
+    
+    init(withName name: String) {
+        
+        self.id = UUID()
+        self.name = name
+    }
 }
