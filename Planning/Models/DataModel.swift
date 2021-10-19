3bdbe910f3f82e2105b900fc0b5e2e6e0af19c58
Alexandre Bintz <alexandre@bintz.io>
2021-10-19 11:21:48 +0200

    add backlog

--- Planning/Models/DataModel.swift
+++ Planning/Models/DataModel.swift
@@ -6,4 +6,5 @@ import Foundation
 struct DataModel: Codable
 {
     let planning: Planning
+    let backlog: Backlog
 }
--- Planning/utils.swift
+++ Planning/utils.swift
@@ -5,7 +5,7 @@ import Foundation
 
 func readDataModel(from url: URL) -> DataModel {
     
-    return (try? JSONDecoder().decode(DataModel.self, from: Data(contentsOf: url))) ?? DataModel(planning: Planning(items: []))
+    return (try? JSONDecoder().decode(DataModel.self, from: Data(contentsOf: url))) ?? DataModel(planning: Planning(items: []), backlog: Backlog(tasks: []))
 }
 
 func save(_ dataModel: DataModel, to url: URL) {
