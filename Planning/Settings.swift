4191aff8168c04771fad3b287346f95f0cda8ef2
Alexandre Bintz <alexandre@bintz.io>
2021-10-19 12:30:08 +0200

    backup data before overwrite

--- Planning/Settings.swift
+++ Planning/Settings.swift
@@ -5,5 +5,8 @@ import Foundation
 
 enum Settings
 {
-    static let fileUrl: URL = URL(fileURLWithPath: FileManager.default.currentDirectoryPath.appending("planning.json"))
+    static let baseFilePath = FileManager.default.currentDirectoryPath.appending("/planning.json")
+    
+    static let fileUrl: URL = URL(fileURLWithPath: baseFilePath)
+    static let backupFileUrl: URL = URL(fileURLWithPath: baseFilePath.replacingOccurrences(of: ".json", with: "-\(DateFormatter.localizedString(from: Date(), dateStyle: .short, timeStyle: .medium).replacingOccurrences(of: "/", with: "-").replacingOccurrences(of: ":", with: "-")).json"))
 }
--- Planning/main.swift
+++ Planning/main.swift
@@ -3,8 +3,13 @@ import Foundation
 
 
 
-let dataModel = readDataModel(from: Settings.fileUrl)
+var dataModel = readDataModel(from: Settings.fileUrl)
+save(dataModel, to: Settings.backupFileUrl)
 
-print(dataModel)
+print(dataModel.backlog)
+print(dataModel.planning)
+
+// modify backlog or planning...
 
 save(dataModel, to: Settings.fileUrl)
+print(Settings.fileUrl)
