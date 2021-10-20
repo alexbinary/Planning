d6553e7899d061f65f1519c49393a77cf1aabb84
Alexandre Bintz <alexandre@bintz.io>
2021-10-20 15:00:37 +0200

    add doc comments on Printer

--- Planning/Print/Printer.swift
+++ Planning/Print/Printer.swift
@@ -3,8 +3,16 @@ import Foundation
 
 
 
+/// A type that computes data that should be presented to the user from model data.
+///
 struct Printer
 {
+    
+    /// Returns the data that should be presented to the user to communicate planning entries on a given optional time slot.
+    ///
+    /// - Parameter dataModel The data model planning entries should be read from
+    /// - Parameter timeSlot If provided, only planning entries that intersect with the time slot are used. Otherwise, all entries are used.
+    ///
     func planningEntryPrintModels(from dataModel: DataModel, on timeSlot: TimeSlot? = nil) -> [PlanningEntryPrintModel] {
         
         var models: [PlanningEntryPrintModel] = []
