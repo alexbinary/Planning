59b08e293d50ea39461577362db0034969ee2447
Alexandre Bintz <alexandre@bintz.io>
2021-10-20 15:00:04 +0200

    add doc comments on PlanningEntryPrintModel

--- Planning/Print/PlanningEntryPrintModel.swift
+++ Planning/Print/PlanningEntryPrintModel.swift
@@ -3,11 +3,40 @@ import Foundation
 
 
 
+/// A type that stores the data that should be presented to the user to communicate a time slot with associated data.
+///
 struct PlanningEntryPrintModel: Equatable
 {
+    
+    /// The time slot that should be printed.
+    ///
     let timeSlot: TimeSlot
+    
+    
+    /// A header text.
+    ///
+    /// Use this to communicate data that refer to the time slot as a whole.
+    ///
     let head: String
+    
+    
+    /// A title text.
+    ///
+    /// Use this to communicate data that is the main focus point amongst the data associated with the time slot.
+    ///
     let title: String
+    
+    
+    /// A subtitle text.
+    ///
+    /// Use this to communicate data of minor importance.
+    ///
     let subtitle: String?
+    
+    
+    /// An extra text
+    ///
+    /// Use this to communicate any extra data associated with the time slot.
+    ///
     let extra: String?
 }
