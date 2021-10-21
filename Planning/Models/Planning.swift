a68a9211e2c88745d0baeba7e1a15cbb9f6ede7f
Alexandre Bintz <alexandre@bintz.io>
2021-10-21 19:22:07 +0200

    add doc comments to type Planning

--- Planning/Models/Planning.swift
+++ Planning/Models/Planning.swift
@@ -3,9 +3,19 @@ import Foundation
 
 
 
+/// A type that represents a planning.
+///
+/// A planning positionnes tasks and other pieces of information on time slots.
+///
 struct Planning: Codable
 {
+    
+    /// The tasks schedulings that the planning stores.
+    ///
     var taskSchedulings: [TaskScheduling]
     
+    
+    /// Returns the date of the task scheduling that ends the most recently.
+    ///
     var mostRecentTaskSchedulingEndDate: Date? { self.taskSchedulings.max(by: { $0.timeSlot.endDate < $1.timeSlot.endDate })?.timeSlot.endDate }
 }
