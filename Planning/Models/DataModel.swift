2001af37085fdd3a3e009292fd3698250e100ac2
Alexandre Bintz <alexandre@bintz.io>
2021-10-26 13:45:55 +0200

    add doc comments to type DataModel

--- Planning/Models/DataModel.swift
+++ Planning/Models/DataModel.swift
@@ -3,10 +3,19 @@ import Foundation
 
 
 
+/// A type that stores all the application's data in a single object.
+///
+/// This type is used to read and write the app's data to and from a file on disk.
+///
 struct DataModel: Codable
 {
-    
+   
+    /// Stores the planning data.
+    ///
     var planning: Planning
     
+    
+    /// Stores the backlog data.
+    ///
     var backlog: Backlog
 }
